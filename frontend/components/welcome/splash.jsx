bf7a5b58cddc98a597ec68f3ed71786fb8709239
shree <shree1795@gmail.com>
2020-01-23 18:22:19 -0500

    Adds styling to contact me, choose a better way to work, and footer sections

--- frontend/components/welcome/splash.jsx
+++ frontend/components/welcome/splash.jsx
@@ -38,21 +38,21 @@ class Splash extends React.Component {
                 <h3>Break out of the inbox</h3>
                 <h4>Working in channels gives everyone on your team a shared view of progress and purpose.</h4>
                 <div className="youtube-slack-vid">
-                    <iframe width="860" height="615" src="https://www.youtube.com/embed/EYqxQGmQkVw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    {/* <iframe width="860" height="615" src="https://www.youtube.com/embed/EYqxQGmQkVw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
                 </div>
                 <div className="breakout-tiles-grid">
                     <div className="breakout-tile">
-                        <i class="far fa-sticky-note"></i>
+                        <i className="far fa-sticky-note"></i>
                         <h5>Organized Conversations</h5>
                         <p>Instead of a single overstuffed inbox, conversations in Tack happen in dedicated spaced called channels.</p>
                     </div>
                     <div className="breakout-tile">
-                        <i class="fas fa-infinity"></i>
+                        <i className="fas fa-infinity"></i>
                         <h5>Get Looped in</h5>
                         <p>Tack makes it simple to follow converstations or find important information in an easily searchable archive.</p>
                     </div>
                     <div className="breakout-tile">
-                       <i class="far fa-hand-pointer"></i>
+                       <i className="far fa-hand-pointer"></i>
                         <h5>Give focus a chance</h5>
                         <p>Unlike email, Tack lets you choose which conversations are most important — and which can wait.</p>
                     </div>
@@ -74,26 +74,36 @@ class Splash extends React.Component {
                 <h3>Contact Me</h3>
                 <div className="contact-me-tiles">
                     <div className="contact-me-tile">
-                        <p>img</p>
-                        <h4>GitHub</h4>
+                       <a href="https://github.com/ShreePatel95" target="_blank">
+                            <div className="github-icon-container"><i className="fab fa-github"></i></div>
+                            <h4>GitHub</h4>
+                        </a>
                     </div>
                    <div className="contact-me-tile">
-                       <p>img</p>
-                       <h4>LinkedIn</h4>
+                       <a href="https://www.linkedin.com/in/shree1795/" target="_blank">
+                            <div className="linkedin-icon-container"><i className="fab fa-linkedin-in"></i></div>
+                            <h4>LinkedIn</h4>
+                       </a>
                    </div>
                    <div className="contact-me-tile">
-                       <p>img</p>
-                       <h4>AngelList</h4>
+                       <a href="https://angel.co/shree-patel" target="_blank">
+                            <div className="angellist-icon-container"><i className="fab fa-angellist"></i></div>
+                            <h4>Angel List</h4>
+                       </a>
                    </div>
                 </div>
             </section>
             <section className="choose-way-to-work">
                 <h3>Choose a better way to work</h3>
-               <div className="slack-demo-links">
-                   <Link className="try-tack-button" to="/signup">Try Tack</Link>
-                   <a className="see-the-demo-button" onClick={this.handleDemoUser}>See the Demo</a>
+               <div className="tack-try-links">
+                   <Link className="try-tack-white-button" to="/signup">Try Tack</Link>
+                   <a className="see-the-demo-white-button" onClick={this.handleDemoUser}>See the Demo</a>
                </div>
             </section>
+            <footer>
+               <i className="fas fa-thumbtack"></i>
+               <p>Built in 2020. Tack is a clone of <a href="https://slack.com/">Slack</a></p>
+            </footer>
         </>);
     };
 };
